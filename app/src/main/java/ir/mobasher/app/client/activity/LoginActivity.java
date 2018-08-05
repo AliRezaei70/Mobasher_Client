b85db501dd3faf514df182a2156eb886304c4223
AliRezaei70 <a.m.rezaei70@gmail.com>
2018-08-05 12:34:42 +0430

    add retrofit2, recycleView and cardView to project

--- app/src/main/java/ir/mobasher/app/client/activity/LoginActivity.java
+++ app/src/main/java/ir/mobasher/app/client/activity/LoginActivity.java
@@ -1,8 +1,9 @@
-package ir.mobasher.app.client;
+package ir.mobasher.app.client.activity;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -31,6 +32,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import ir.mobasher.app.client.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -86,7 +89,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+               // attemptLogin();
             }
         });
 
