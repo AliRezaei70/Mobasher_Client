09bbaf737b189749ddb4e540b624728d305952c6
AliRezaei70 <a.m.rezaei70@gmail.com>
2019-02-27 11:43:46 +0330

    add navigation drawer

--- app/src/main/java/ir/mobasher/app/client/intreface/GetDataService.java
+++ app/src/main/java/ir/mobasher/app/client/intreface/GetDataService.java
@@ -8,6 +8,6 @@ import retrofit2.http.GET;
 
 public interface GetDataService {
 
-    @GET("/photos")
+    @GET("/api/v1/client/packs/111c7528-1d44-4ae3-9dc0-c3b8213d45a6")
     Call<List<RetroPhoto>> getAllPhotos();
 }
--- app/src/main/java/ir/mobasher/app/client/network/RetrofitClientInstance.java
+++ app/src/main/java/ir/mobasher/app/client/network/RetrofitClientInstance.java
@@ -1,17 +1,18 @@
 package ir.mobasher.app.client.network;
 
+import ir.mobasher.app.client.app.Config;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
 
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+    //private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(Config.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
